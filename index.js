a5b0f3f24a1136d85245921293293bfafdeeabb6
Ricardo dos Santos Souza <ricardo.tecnology@gmail.com>
2018-11-23 19:19:04 -0200

    trocando para a porta 3000 - deploy umbler

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const genres   = require('./routes/genres');
 const clients  = require('./routes/clients');
 const movies   = require('./routes/movies');
 const app      = express();
-const port     = 5000;
+const port     = 3000;
 
 app.use(express.json());
 app.use('/api/genres',  genres);
