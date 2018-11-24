bfb4ff0614b0256294f2df8fb685651b6f5b19c5
Ricardo dos Santos Souza <ricardo.tecnology@gmail.com>
2018-11-24 17:43:21 -0200

    ajustando banco de produção e banco local

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 const Joi      = require('joi');
 const express  = require('express');
-// const mongoose = require('mongoose'); 
-// const genres   = require('./routes/genres');
-// const clients  = require('./routes/clients');
-// const movies   = require('./routes/movies');
+const mongoose = require('mongoose'); 
+const genres   = require('./routes/genres');
+const clients  = require('./routes/clients');
+const movies   = require('./routes/movies');
 const app      = express();
 const port     = process.env.PORT || 3000;
 
 app.use(express.json());
-// app.use('/api/genres',  genres);
-// app.use('/api/clients', clients);
-// app.use('/api/movies',  movies);
+app.use('/api/genres',  genres);
+app.use('/api/clients', clients);
+app.use('/api/movies',  movies);
 
 // Setando propiedades para cada ambiente
-// if(process.env.NODE_ENV !== undefined) {
-// 	mongoose.connect('mongodb://localhost/vidly')
-// 		.then(() => console.log('connected to the database'))
-// 		.catch((err) => console.log(err));	
-// } else {
-// 	mongoose.connect('mongodb://localhost/ricardo')
-// 		.then(() => console.log('connected to the database'))
-// 		.catch((err) => console.log(err));		
-// }
+console.log(process.env.NODE_ENV);
+
+if(process.env.NODE_ENV == 'development') {
+	mongoose.connect('mongodb://localhost/vidly')
+		.then(() => console.log('connected to the database'))
+		.catch((err) => console.log(err));	
+} else {	
+	const options = {
+	    user: 'ricardo_souza',
+	    pass: 'develop1990',
+	}
+	const uri = 'mongodb://tatooine.mongodb.umbler.com:36927/apiDb';
+	
+	mongoose.connect(uri, options)
+		.then(() => console.log('connected to the database in umbler'))
+		.catch((err) => console.log(err));		
+}
 
 app.get('/', (req, res) => {
 	res.send('Está rodando na umbler');
