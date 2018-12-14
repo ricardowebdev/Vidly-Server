38f3109a6d0c5967c71f47fdc989f3051f284ea3
Ricardo dos Santos Souza <ricardo.tecnology@gmail.com>
2018-12-14 08:46:00 -0200

    implementado password hash e login

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const express  = require('express');
 const router   = express.Router();
+const _        = require('lodash');
+const bcrypt   = require('bcrypt');
 const { User, validate } = require('../models/users');
 
 router.get('/', async (req, res) => {
@@ -40,18 +42,15 @@ router.post('/', async (req, res) => {
 		return res.status(400).send(error.details[0].message);
 
 	try {		
-		const user = new User({
-			name:      req.body.name,
-			email:     req.body.email,
-			password:  req.body.password,
-		});
-
-		const result = await user.save()
+		const user    = new User(_.pick(req.body, ['name', 'email', 'password']));
+		const salt    = await bcrypt.genSalt(10);
+		user.password = await bcrypt.hashSync(user.password, salt);
+		const result  = await user.save()
 
 		if (!result)
 			throw new Error('Bad Request...');	
 		
-		res.send(result);
+		res.send(_.pick(user, ['_id', 'name', 'email']));
 	} catch(err) {
 		res.status(400).send(err.message);
 	}
@@ -78,7 +77,7 @@ router.put('/:id', async(req, res) => {
 		if(!result)
 			throw new Error("Bad request while saving new User");
 
-		res.send(user);
+		res.send(_.pick(user, ['_id', 'name', 'email']));
 
 	} catch(err) {
 		res.status(404).send(err.message);
@@ -101,6 +100,25 @@ router.delete('/:id', async(req, res) => {
 		res.send(user);
 	} catch(err) {
 		res.status(404).send(err.message);
+	}
+});
+
+router.post('/login', async(req, res) => {
+	const email    = req.body.email    || null;
+	const password = req.body.password || null;
+
+	try {		
+		const user = await User.findOne({ email: email });
+		if (!user)
+			throw new Error('User or e-mail not valid');
+			
+		const result = await bcrypt.compare(password, user.password);			
+		if (!result)
+			throw new Error('User or e-mail not valid');
+		
+		res.send(result);
+	} catch(err) {
+		res.status(401).send(err.message);
 	}
 });
 
